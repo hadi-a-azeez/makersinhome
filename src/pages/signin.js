04b84b34fd0170359f0bfe9e200dee1baede0a6d
Hadi Azeez <62507812+hadi-a-azeez@users.noreply.github.com>
2020-09-21 23:09:25 +0530

    Update signin.js

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./css/signup.module.css";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 
 const SignIn = () => {
+  const [loginUsername, setLoginUsername] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  let history = useHistory();
+  const signIn = () => {
+    axios({
+      method: "post",
+      data: {
+        phone: loginUsername,
+        password: loginPassword,
+      },
+
+      url: "https://shopwhats-backend.herokuapp.com/login",
+    }).then(function (response) {
+      console.log(response);
+      if (response.data.message.login === true) {
+        console.log("need to be redirected");
+        history.push("/dashboard");
+      }
+    });
+  };
   return (
     <div className={styles.container}>
       <div className={styles.heading_block}>
@@ -12,13 +34,17 @@ const SignIn = () => {
           type="text"
           className={styles.input_field}
           placeholder="Whatsapp no"
+          onChange={(e) => setLoginUsername(e.target.value)}
         />
         <input
           type="password"
           className={styles.input_field}
           placeholder="Password"
+          onChange={(e) => setLoginPassword(e.target.value)}
         />
-        <button className={styles.btn}>Sign in</button>
+        <button className={styles.btn} onClick={signIn}>
+          Sign in
+        </button>
       </div>
     </div>
   );
