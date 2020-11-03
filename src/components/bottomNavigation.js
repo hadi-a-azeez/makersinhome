2c018d8460a9b91423f556df01ca22308e9f550b
hadi-a-azeez <hadikoduvally@gmail.com>
2020-11-03 23:09:04 +0530

    ui fix

--- src/components/bottomNavigation.js
+++ src/components/bottomNavigation.js
@@ -1,35 +1,56 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./bottomNavigation.module.css";
-import { makeStyles } from "@material-ui/core/styles";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
-/* import HomeIcon from "@material-ui/icons/HomeOutlined"; */
-/* import LayersIcon from "@material-ui/icons/LayersOutlined";
-import DashboardIcon from "@material-ui/icons/DashboardOutlined";
-import PersonIcon from "@material-ui/icons/PersonOutlined"; */
 import { Link } from "react-router-dom";
 import HomeIcon from "../assets/home-outline.svg";
+import HomeIconFilled from "../assets/homeFilled.svg";
 import ProductsIcon from "../assets/layers-outline.svg";
+import ProductsIconFilled from "../assets/layersFilled.svg";
 import CategoriesIcon from "../assets/grid-outline.svg";
+import CategoriesIconFilled from "../assets/gridFilled.svg";
 import AccountIcon from "../assets/person-outline.svg";
+import AccountIconFilled from "../assets/personFilled.svg";
 
-const useStyles = makeStyles({
-  root: {
-    width: 0,
-  },
-  element: {
-    width: "100%",
-    padding: 0,
-    margin: "100%",
-    color: "#767676",
-    "&$selected": {
-      
-    },
-  },
-});
 const BottomNavigationMenu = () => {
-  const classes = useStyles();
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState("home");
+  const [isHomeSelected, setIsHomeSelected] = useState(true);
+  const [isProductsSelected, setIsProductsSelected] = useState(false);
+  const [isCategoriesSelected, setIsCategorieSelected] = useState(false);
+  const [isAccountSelected, setIsAccountSelected] = useState(false);
+
+  useEffect(() => {
+    switch (value) {
+      case "home":
+        setIsHomeSelected(true);
+        setIsProductsSelected(false);
+        setIsCategorieSelected(false);
+        setIsAccountSelected(false);
+        break;
+      case "products":
+        setIsHomeSelected(false);
+        setIsProductsSelected(true);
+        setIsCategorieSelected(false);
+        setIsAccountSelected(false);
+        break;
+      case "categories":
+        setIsHomeSelected(false);
+        setIsProductsSelected(false);
+        setIsCategorieSelected(true);
+        setIsAccountSelected(false);
+        break;
+      case "account":
+        setIsHomeSelected(false);
+        setIsProductsSelected(false);
+        setIsCategorieSelected(false);
+        setIsAccountSelected(true);
+        break;
+    
+      default:
+        break;
+    }
+  }, [value]);
+  
 
   return (
     <>
@@ -37,40 +58,51 @@ const BottomNavigationMenu = () => {
         <nav className={styles.bottom_nav}>
           <BottomNavigation
             showLabels
-            className={classes.root}
+            className={styles.navItem}
             value={value}
             onChange={(event, value) => {
               setValue(value);
-              console.log(value)
             }}
           >
             <BottomNavigationAction
-              className={classes.element}
+              className={isHomeSelected ? styles.navItemSelected 
+                : styles.navItem}
               label="Home"
-              icon={<img src={HomeIcon} alt="home" className={styles.iconstyle} />}
+              icon={isHomeSelected ? <img src={HomeIconFilled} alt="home" className={styles.iconFilled} /> 
+              : <img src={HomeIcon} alt="home" className={styles.iconOutlined} />}
               component={Link}
               to="/dashboard"
+              value="home"
             />
             <BottomNavigationAction
-              className={classes.element}
+              className={isProductsSelected ? styles.navItemSelected 
+                : styles.navItem}
               label="Products"
-              icon={<img src={ProductsIcon} alt="home" className={styles.iconstyle} />}
+              icon={isProductsSelected ? <img src={ProductsIconFilled} alt="home" className={styles.iconFilled} /> 
+              : <img src={ProductsIcon} alt="home" className={styles.iconOutlined} />}
               component={Link}
               to="/products"
+              value="products"
             />
             <BottomNavigationAction
-              className={classes.element}
+              className={isCategoriesSelected ? styles.navItemSelected 
+                : styles.navItem}
               label="Categories"
-              icon={<img src={CategoriesIcon} alt="home" className={styles.iconstyle} />}
+              icon={isCategoriesSelected ? <img src={CategoriesIconFilled} alt="home" className={styles.iconFilled} /> 
+              : <img src={CategoriesIcon} alt="home" className={styles.iconOutlined} />}
               component={Link}
               to="/categories"
+              value="categories"
             />
             <BottomNavigationAction
               label="Account"
-              icon={<img src={AccountIcon} alt="home" className={styles.iconstyle} />}
-              className={classes.element}
+              icon={isAccountSelected ? <img src={AccountIconFilled} alt="home" className={styles.iconFilled} /> 
+              : <img src={AccountIcon} alt="home" className={styles.iconOutlined} />}
+              className={isAccountSelected ? styles.navItemSelected 
+                : styles.navItem}
               component={Link}
               to="/account"
+              value="account"
             />
           </BottomNavigation>
         </nav>
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -2,41 +2,49 @@ import React from "react";
 import styles from "./css/dashboard.module.css";
 import { Link } from "react-router-dom";
 import LabelHeader from "../components/labelHeader";
+import VisibleIcon from "../assets/eye.svg";
+import MessagesIcon from "../assets/chatbubble-ellipses.svg";
+import ProductsIcon from "../assets/layersFilled.svg";
+import CategoriesIcon from "../assets/gridFilled.svg";
 
 const Dashboard = () => {
   return (
     <>
       <div className={styles.container}>
-      <LabelHeader label={"Dashboard"} />
-        <Link to="/add_product" className={styles.btn}>
+      <LabelHeader label={"Home"} />
+        {/* <Link to="/add_product" className={`${styles.btn}`}>
           ADD PRODUCTS
-        </Link>
+        </Link> */}
         <div className={styles.heading_block}>
           <h1 className={styles.heading_bold}>Dashboard</h1>
         </div>
         <div className={styles.row}>
-          <div className={styles.card}>
+          <div className={`${styles.card} ${styles.green}`}>
             <div className={styles.card_content}>
+              <img src={VisibleIcon} alt="home" className={styles.iconFilled} />
               <h1 className={styles.card_heading}>Store visits</h1>
               <h1 className={styles.card_data_bold}>299</h1>
             </div>
           </div>
-          <div className={styles.card}>
+          <div className={`${styles.card} ${styles.blue}`}>
             <div className={styles.card_content}>
+              <img src={MessagesIcon} alt="home" className={styles.iconFilled} />
               <h1 className={styles.card_heading}>Messages started</h1>
               <h1 className={styles.card_data_bold}>78</h1>
             </div>
           </div>
         </div>
         <div className={styles.row}>
-          <div className={styles.card}>
+          <div className={`${styles.card} ${styles.yellow}`}>
             <div className={styles.card_content}>
+            <img src={ProductsIcon} alt="home" className={styles.iconFilled} />
               <h1 className={styles.card_heading}>Total products</h1>
               <h1 className={styles.card_data_bold}>18</h1>
             </div>
           </div>
-          <div className={styles.card}>
+          <div className={`${styles.card} ${styles.red}`}>
             <div className={styles.card_content}>
+            <img src={CategoriesIcon} alt="home" className={styles.iconFilled} />
               <h1 className={styles.card_heading}>Categories</h1>
               <h1 className={styles.card_data_bold}>6</h1>
             </div>
