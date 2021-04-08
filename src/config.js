8b3f5e7deaee76998d65d7905cf60151b113efc6
muhammednasimda <muhammednasimda@gmail.com>
2021-04-08 15:51:52 +0530

    changed cnfig

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-// export const apiRoot = "https://wecollab.club/api";
+export const apiRoot = "https://wecollab.club/api";
 export const productImagesRoot = "https://wecollab.club/api/product-images";
 export const profileImagesRoot = "https://wecollab.club/api/profile-images";
 
-export const apiRoot = "http://localhost:5000/api";
+//export const apiRoot = "http://localhost:5000/api";
 // export const profileImagesRoot = "http://localhost:5000/api/profile-images";
 // export const productImagesRoot = "http://localhost:5000/api/product-images";
