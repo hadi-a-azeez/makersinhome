83ffb2ed2cb8ec16fa33ae33df764ce1099627b2
hadi-a-azeez <hadikoduvally@gmail.com>
2021-05-17 19:59:02 +0530

    making home page responsive

--- src/pages/client/home.js
+++ src/pages/client/home.js
@@ -13,66 +13,69 @@ const Home = () => {
   const history = useHistory();
 
   return (
-    <div className={styles.container}>
+    <>
       <Header />
-      <h1 className={styles.main_heading}>
-        Build an online store for your Instagram business .
-      </h1>
-      <h1 className={styles.sub_heading}>
-        An online store for your Instagram business will make you stand alone
-        among your competitors.
-      </h1>
-      <button
-        className={styles.register_btn}
-        onClick={() => history.push("/signup")}
-      >
-        Create My Store
-      </button>
-      <Text
-        mt="12px"
-        alignSelf="center"
-        color="#008aed"
-        fontSize="15px"
-        fontFamily="elemen"
-        borderBottom="1.5px solid #008aed"
-        onClick={() => history.push("/store/jungle")}
-      >
-        See a Example Store
-      </Text>
-      <Stack
-        onClick={() =>
-          window.location.replace(
-            `https://api.whatsapp.com/send?phone=919496742190&text=Hi%20i%20have%20some%20doubts%20about%20Saav%20%E2%9C%8B`
-          )
-        }
-        position="fixed"
-        bottom="20px"
-        right="20px"
-      >
-        <IconButton
-          alignSelf="flex-end"
-          className={styles.whatsapp_button}
-          icon={<img src={Whatsapp} alt="whatsapp" width="35px" />}
-          boxSize="50px"
-          borderRadius="full"
-        />
-        <Badge
-          backgroundColor="yellow.300"
-          paddingRight="8px"
-          paddingLeft="8px"
-          borderRadius="12px"
+      <div className={styles.container}>
+        <div className={styles.gridLeft}>
+          <h1 className={styles.main_heading}>
+            Build an online store for your Instagram business.
+          </h1>
+          <h1 className={styles.sub_heading}>
+            An online store for your Instagram business will make you stand
+            alone among your competitors.
+          </h1>
+          <button
+            className={styles.register_btn}
+            onClick={() => history.push("/signup")}
+          >
+            Create My Store
+          </button>
+          <Text
+            mt="12px"
+            color="#008aed"
+            fontSize="15px"
+            fontFamily="elemen"
+            borderBottom="1.5px solid #008aed"
+            onClick={() => history.push("/store/jungle")}
+            className={styles.example_link}
+          >
+            See an Example Store
+          </Text>
+        </div>
+        <Stack
+          onClick={() =>
+            window.location.replace(
+              `https://api.whatsapp.com/send?phone=919496742190&text=Hi%20i%20have%20some%20doubts%20about%20Saav%20%E2%9C%8B`
+            )
+          }
+          className={styles.whatsappbtn_wraper}
         >
-          {" "}
-          Message Us
-        </Badge>
-      </Stack>
-      <img src={mockupmain} alt="storedemo" className={styles.main_mockup} />
-      <div>
-        <div></div>
-        <div></div>
-        <div></div>
+          <IconButton
+            alignSelf="flex-end"
+            className={styles.whatsapp_button}
+            icon={<img src={Whatsapp} alt="whatsapp" width="35px" />}
+            boxSize="50px"
+            borderRadius="full"
+          />
+          <Badge
+            backgroundColor="yellow.300"
+            paddingRight="8px"
+            paddingLeft="8px"
+            borderRadius="12px"
+          >
+            {" "}
+            Message Us
+          </Badge>
+        </Stack>
+        <div className={styles.gridRight}>
+          <img
+            src={mockupmain}
+            alt="storedemo"
+            className={styles.main_mockup}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
