8df35c8ac0e923bf0bb04d0363e5331c81fd78ec
hadi-a-azeez <hadikoduvally@gmail.com>
2021-05-19 16:49:12 +0530

    changed to cdn

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const apiRoot = "https://wecollab.club/api";
 export const productImagesRoot =
-  "https://saav.sgp1.digitaloceanspaces.com/product-images";
+  "https://saav.sgp1.cdn.digitaloceanspaces.com/product-images";
 export const profileImagesRoot =
-  "https://saav.sgp1.digitaloceanspaces.com/profile-images";
+  "https://saav.sgp1.cdn.digitaloceanspaces.com/profile-images";
 
 //export const apiRoot = "http://localhost:5000/api";
