1c90443b1e00c1c22d0792ed6667bd7492f41361
hadi-a-azeez <hadikoduvally@gmail.com>
2021-06-03 22:05:45 +0530

    fixed bug

--- src/components/nux.js
+++ src/components/nux.js
@@ -1,4 +1,4 @@
-//import React, { useState, useEffect } from "react";
+import React from "react";
 import styles from "./css/nux.module.css";
 import Tick from "../assets/checkmark.svg";
 import { useHistory } from "react-router-dom";
