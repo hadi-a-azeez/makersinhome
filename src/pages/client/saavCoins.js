b0fa792c01d1d492b18b832d9609c0a0b77493ff
muhammednasimda <muhammednasimda@gmail.com>
2021-06-27 17:33:44 +0530

    small change

--- src/pages/client/saavCoins.js
+++ src/pages/client/saavCoins.js
@@ -4,7 +4,7 @@ import { Box, Heading, Text, Stack } from "@chakra-ui/layout";
 const SaavCoins = () => {
   return (
     <Stack>
-      <Heading>Coins</Heading>
+      <Heading>Coins is working</Heading>
     </Stack>
   );
 };
