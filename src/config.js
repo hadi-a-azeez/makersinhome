cad0c81a1d7b6811ab2f34fa81d77773ff78669f
muhammednasimda <muhammednasimda@gmail.com>
2021-10-21 12:00:55 +0530

    chnaged localhost

--- src/config.js
+++ src/config.js
@@ -1,8 +1,6 @@
-//export const apiRoot = "https://wecollab.club/api";
-//export const apiRoot = "https://saav.shop/api";
 export const productImagesRoot =
   "https://saav.sgp1.cdn.digitaloceanspaces.com/product-images";
 export const profileImagesRoot =
   "https://saav.sgp1.cdn.digitaloceanspaces.com/profile-images";
 
-export const apiRoot = "http://localhost:5000/api";
+export const apiRoot = "https://saav.shop/api";
