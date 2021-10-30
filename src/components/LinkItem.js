1ce84fa9bc594afcd0cae4605e1512aba665e7aa
muhammednasimda <muhammednasimda@gmail.com>
2021-10-30 13:27:30 +0530

    made ui

---
+++ src/components/LinkItem.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+
+import { Stack, Button, Heading, Image, Text, Box } from "@chakra-ui/react";
+
+import { DragHandleIcon } from "@chakra-ui/icons";
+import { Draggable } from "react-beautiful-dnd";
+
+const LinkItem = ({ index, item }) => {
+  return (
+    <Draggable key={item.id} draggableId={item.id.toString()} index={index}>
+      {(provided) => (
+        <Stack
+          spacing="0"
+          key={item.id}
+          background="white"
+          direction="row"
+          borderRadius="6px"
+          mb="10px"
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          style={provided.draggableProps.style}
+          w="100%"
+        >
+          <Stack
+            direction="row"
+            alignItems="center"
+            minWidth="85%"
+            maxWidth="85%"
+          >
+            <Image w="25%" p="15px" pr="2px" src={item.image} />
+            <Stack
+              direction="column"
+              spacing="0"
+              justifyContent="center"
+              w="65%"
+            >
+              <Heading
+                size="md"
+                whiteSpace="nowrap"
+                textOverflow="ellipsis"
+                overflow="hidden"
+              >
+                {item.title}
+              </Heading>
+              <Text
+                whiteSpace="nowrap"
+                textOverflow="ellipsis"
+                overflow="hidden"
+              >
+                {item.url}
+              </Text>
+            </Stack>
+          </Stack>
+          <Box
+            {...provided.dragHandleProps}
+            w="15%"
+            display="grid"
+            placeItems="center"
+            background="#dedede"
+            borderRadius="0px 6px 6px 0px"
+          >
+            <DragHandleIcon boxSize="20px" />
+          </Box>
+        </Stack>
+      )}
+    </Draggable>
+  );
+};
+
+export default LinkItem;
--- src/pages/seller/links/index.js
+++ src/pages/seller/links/index.js
@@ -1,19 +1,101 @@
 import React, { useState, useEffect } from "react";
 import styles from "../css/products.module.css";
 import { useHistory } from "react-router-dom";
-
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { Stack, Button, Heading, Box, Text } from "@chakra-ui/react";
 import LabelHeader from "../../../components/labelHeader";
 import BottomNavigationMenu from "../../../components/bottomNavigation";
-const Links = (props) => {
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { produce } from "immer";
+import LinkItem from "../../../components/LinkItem";
+
+const Links = () => {
   const [isLoading, setIsLoading] = useState(false);
   let history = useHistory();
+  const [links, setLinks] = useState([
+    {
+      id: 1,
+      title: "Link 1",
+      url: "https://www.google.comssssssssssssssssssssssss",
+      image: "https://picsum.photos/200",
+      position: 1,
+    },
+    {
+      id: 2,
+      title: "Link 2",
+      url: "https://www.google.com",
+      image: "https://picsum.photos/200",
+      position: 2,
+    },
+    {
+      id: 3,
+      title: "Link 3",
+      url: "https://www.google.com",
+      image: "https://picsum.photos/200",
+      position: 3,
+    },
+  ]);
+
+  useEffect(() => {
+    let newLinks = links.map((link) => {
+      return { ...link, position: links.indexOf(link) + 1 };
+    });
+    console.log(newLinks);
+  }, [links]);
+
+  const reorder = (sourceIndex, destinationIndex) => {
+    setLinks(
+      produce(links, (draft) => {
+        const [removed] = draft.splice(sourceIndex, 1);
+        draft.splice(destinationIndex, 0, removed);
+      })
+    );
+  };
+  const onDragEnd = (result) => {
+    console.log("called");
+    //dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    reorder(result.source.index, result.destination.index);
+  };
 
   return (
     <>
       <div className={styles.container}>
         <LabelHeader label="Links" />
-        Limks
+        <Stack w="100%" pl="6%" pr="6%" direction="column">
+          <Stack direction="row" mt="8">
+            <Button
+              backgroundColor="#08bd80"
+              colorScheme="green"
+              color="white"
+              size="lg"
+              w="70%"
+            >
+              Add Link
+            </Button>
+            <Button backgroundColor="white" size="lg" w="30%">
+              + Title
+            </Button>
+          </Stack>
+          <Heading pt="20px" size="lg">
+            My links
+          </Heading>
+          <DragDropContext onDragEnd={onDragEnd}>
+            {
+              <Droppable droppableId="droppable">
+                {(provided) => (
+                  <Box {...provided.droppableProps} ref={provided.innerRef}>
+                    {links.map((item, i) => (
+                      <LinkItem item={item} key={item.id} index={i} />
+                    ))}
+                  </Box>
+                )}
+              </Droppable>
+            }
+          </DragDropContext>
+        </Stack>
         <BottomNavigationMenu />
       </div>
     </>
