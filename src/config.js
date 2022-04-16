cac741430d0982849ba3589d6a2dcd4eebc5d9ab
hadi-a-azeez <hadikoduvally@gmail.com>
2022-04-16 20:47:57 +0530

    loclahost changed

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ export const productImagesRoot =
 export const profileImagesRoot =
   "https://saav-s3-bucket.s3.ap-south-1.amazonaws.com/profile-images";
 
-// export const apiRoot = "https://api.saav.shop/api";
-export const apiRoot = "http://localhost:5000/api";
+export const apiRoot = "https://api.saav.shop/api";
+// export const apiRoot = "http://localhost:5000/api";
